f8ef6084de6875caf3d0dcc6284e29398331da9c
Ari Ganbot <ariganbot@gmail.com>
2023-05-27 11:43:44 -0400

    Just demonstrating how to use github copilot

--- fifthpython.py
+++ fifthpython.py
@@ -1,1 +1,20 @@
 print('will the madness never end?')
+# Let's count to ten
+for i in range(1, 11):
+    print(i)    # This is the body of the loop  
+print('we are done')    # This is outside the loop
+   # now let's clear the screen
+
+# Clear the screen
+print('\n' * 100)
+
+# Get the current date from the operating system
+import datetime
+now = datetime.datetime.now()
+print(now)
+
+# Extract the hour of the day from the date
+hour = now.hour
+print(hour)
+
+
